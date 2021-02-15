ee470f6894fd7ccde01e6cb0232e705527c9fc18
Adem Atici <adematicis41@gmail.com>
2021-02-15 23:34:00 +0300

    Genel görünümler oluşturuldu.

--- app/src/main/java/com/adematici/weatherapp/MainActivity.kt
+++ app/src/main/java/com/adematici/weatherapp/MainActivity.kt
@@ -2,10 +2,26 @@ package com.adematici.weatherapp
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import androidx.navigation.findNavController
+import androidx.navigation.ui.AppBarConfiguration
+import androidx.navigation.ui.setupActionBarWithNavController
+import androidx.navigation.ui.setupWithNavController
+import com.adematici.weatherapp.databinding.ActivityMainBinding
 
 class MainActivity : AppCompatActivity() {
+
+    private lateinit var binding: ActivityMainBinding
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_main)
+        binding = ActivityMainBinding.inflate(layoutInflater)
+        setContentView(binding.root)
+
+        val navController = findNavController(R.id.fragment)
+        val appBarConfiguration = AppBarConfiguration(
+            setOf(R.id.homeFragment,R.id.detayFragment,R.id.ayarlarFragment))
+        setupActionBarWithNavController(navController,appBarConfiguration)
+
+        binding.bottomNavView.setupWithNavController(navController)
     }
 }
